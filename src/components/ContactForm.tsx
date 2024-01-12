cb029b57d4301d6469d2157f212ac48d05eab50b
Stoil100 <toli100ilov@gmail.com>
2024-01-12 22:55:31 +0200

    Feat: Create contact form component

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,133 @@
+"use client";
+import * as z from "zod";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+    name: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+    email: z.string().email({ message: "Please enter a valid email address." }),
+    phone: z
+        .string()
+        .regex(
+            new RegExp(
+                /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/,
+            ),
+            "Invalid phone number",
+        )
+        .optional(),
+    message: z
+        .string()
+        .min(5, {
+            message: "Username must be at least 5 characters.",
+        })
+        .max(1000, {
+            message: "Message must not be more than 1000 characters.",
+        }),
+});
+
+export default function ContactForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            message: "",
+        },
+    });
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex h-full w-full flex-col justify-between p-10 text-black"
+            >
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name:</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your name here..."
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email:</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your email here..."
+                                    {...field}
+                                />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="phone"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Phone:</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your phone here..."
+                                    {...field}
+                                />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="message"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Message:</FormLabel>
+                            <FormControl>
+                                <textarea
+                                    className="w-full rounded-md border-2 p-1 text-gray-500"
+                                    placeholder="Enter your message here..."
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button className="w-full" type="submit">
+                    Submit
+                </Button>
+            </form>
+        </Form>
+    );
+}
