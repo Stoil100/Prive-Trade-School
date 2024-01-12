421320860d6f028d4adaee2c658ca7c052d933d0
Stoil100 <toli100ilov@gmail.com>
2024-01-12 22:57:04 +0200

    Feat: Create new design for top bar

--- src/components/TopSocialMedia.tsx
+++ src/components/TopSocialMedia.tsx
@@ -1,25 +1,30 @@
 import React from "react";
-import { AtSign, Facebook, Instagram, PhoneCall, Twitter } from "lucide-react";
+import {
+    AtSign,
+    MapPin,
+    PhoneCall,
+    Search,
+} from "lucide-react";
 
 export default function TopSocialMedia() {
     return (
-        <div className="h-[32px] max-w-full items-center bg-gradient-to-r from-[#4576F7] to-[#0606cc] p-2 text-center text-white flex lg:w-screen justify-between lg:px-12 lg:py-2">
-            <div className="flex flex-col items-start justify-center w-1/2 lg:w-1/4">
-                <div className="flex gap-2 items-center">
-                    <PhoneCall  size={15}/>
-                    <p className="text-xs">+359 000 000 000</p>
-                </div>
-                <div className="flex gap-2 items-center">
-                    <AtSign size={15}/>
-                    <p className="text-xs">test@gmail.com</p>
-                </div>
-                <div></div>
+        <div className="flex md:h-[32px] text-xs flex-wrap items-center justify-around bg-white text-blue-500 p-2 text-center lg:w-screen lg:px-20 lg:py-2 gap-2">
+            <div className="flex items-center gap-2">
+                <MapPin size={15}/>
+                <p>ул. „Преспа“ 1, 9000 Гръцки квартал, гр. Варна</p>
             </div>
-            <h5 className="hidden lg:block">Официален сайт на Часнта търговска гимназия - гр. Варна</h5>
-            <div className="flex w-1/4 justify-end gap-3">
-                <Facebook />
-                <Instagram />
-                <Twitter/>
+            <div className="flex items-center gap-2">
+                <PhoneCall size={15} />
+                <p>+359 000 000 000</p>
+            </div>
+            <div className="flex items-center gap-2">
+                <AtSign size={15} />
+                <p>test@gmail.com</p>
+            </div>
+            <div className="flex items-center gap-3">
+                <Search size={15}/>
+                <p>EN</p>
+                <p>BG</p>
             </div>
         </div>
     );
