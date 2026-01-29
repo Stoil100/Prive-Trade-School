251ab31045a4d477566ddb41c757f0cf457c8e7e
Stoil100 <toli100ilov@gmail.com>
2026-01-29 17:07:06 +0200

    feat: Add outage overlay component and update layout to handle outages

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,12 +5,14 @@ import Navigation from "@/components/Navigation";
 import { ParallaxProvider } from "@/components/providers/parallax";
 import TopSocialMedia from "@/components/TopSocialMedia";
 import { cn } from "@/lib/utils";
-import { NextIntlClientProvider, useMessages } from "next-intl";
+import { NextIntlClientProvider } from "next-intl";
 import { Inter as FontSans } from "next/font/google";
 import { Toaster } from "sonner";
 import { AuthContextProvider } from "../../components/providers/auth";
 import "./global.css";
 import Favicon from "/public/favicon.ico";
+import OutageOverlay from "@/components/Outage";
+import { getMessages } from "next-intl/server";
 export const fontSans = FontSans({
     subsets: ["latin"],
     variable: "--font-sans",
@@ -22,31 +24,36 @@ export const metadata: Metadata = {
     icons: [{ rel: "icon", url: Favicon.src }],
 };
 
-export default function LocaleLayout({
+export default async function LocaleLayout({
     children,
     params: { locale },
 }: {
     children: React.ReactNode;
     params: { locale: string };
 }) {
-    const messages = useMessages();
+    const messages = await getMessages();
+    const outage=true;
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="bg" suppressHydrationWarning>
             <head />
             <body
                 className={cn(
                     "flex min-h-screen w-screen max-w-full flex-col overflow-x-hidden scroll-smooth bg-gray-900 font-montserrat antialiased",
                 )}
             >
-                <NextIntlClientProvider messages={messages} locale={locale}>
-                    <AuthContextProvider>
-                        <TopSocialMedia />
-                        <Navigation />
-                        <ParallaxProvider>{children}</ParallaxProvider>
-                        <Footer />
-                        <Toaster />
-                    </AuthContextProvider>
-                </NextIntlClientProvider>
+                {outage ? (
+                    <OutageOverlay />
+                ) : (
+                    <NextIntlClientProvider messages={messages} locale={locale}>
+                        <AuthContextProvider>
+                            <TopSocialMedia />
+                            <Navigation />
+                            <ParallaxProvider>{children}</ParallaxProvider>
+                            <Footer />
+                            <Toaster />
+                        </AuthContextProvider>
+                    </NextIntlClientProvider>
+                )}
             </body>
         </html>
     );
